8859b71fade825fe01fd992477bab0dfcc93ea44
jesusmorales793 <105897626+jesusmorales793@users.noreply.github.com>
2024-03-02 12:34:15 -0600

    initial verification

--- Register/registration.js
+++ Register/registration.js
@@ -1,8 +1,24 @@
+function userCreation(){
+    const newUsers = {
+  users: [
+    {
+      email: "luis@gmail.com",
+      password: "1234",
+    }
+  ],
+};
+
+localStorage.setItem("usersDB", JSON.stringify(newUsers));
+}
+
+
 
 function register(){
 
-    
+
     const userEmail = document.getElementById("email").value;
+
+    validateRegistration(userEmail);
 
     const userName = document.getElementById("name").value;
 
@@ -10,22 +26,28 @@ function register(){
 
     const user = {email:userEmail, name:userName, password:userPassword}
 
-    validateRegistration(userEmail);
+    const prevUsers = JSON.parse(localStorage.getItem("usersDB")).users;
+
+    const newUsers = [...prevUsers,user]
+
+    localStorage.setItem("usersDB", JSON.stringify(newUsers))
 
 }
 
 function validateRegistration(email){
-    
-    const users = JSON.parse(localStorage.getItem("users")).users;
 
-    const usersString = JSON.stringify(users);
+    const users = JSON.parse(localStorage.getItem("usersDB")).users;
 
-    console.log(users.users[0].email);
+    // const emailVerification = users.find((user) => user.email == email);
 
+    // localStorage.setItem("verifyEmail", JSON.stringify(emailVerification))
+
+    console.log("hola")
 
 }
 
 
-    
+
+
 
 
