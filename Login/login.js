dc9a82ea6395161172a1423d1b5492083afa84a7
Luis Fuentes Fuentes <80366974+L4F7@users.noreply.github.com>
2024-03-14 19:15:05 -0600

    validate user in db

--- Login/login.js
+++ Login/login.js
@@ -1,46 +1,27 @@
-function validateUser(email, password) {
-  const users = JSON.parse(localStorage.getItem('users')).users;
-
-  const checkData = (user) =>
-    user.email === email && user.password === password;
-
-  return users.some(checkData);
+function initUsersDB() {
+  if (!localStorage.getItem('usersDB')) {
+    localStorage.setItem('usersDB', JSON.stringify([]));
+  }
 }
 
-document
-  .getElementById('loginForm')
-  .addEventListener('submit', event => {
-    event.preventDefault(); // Prevent the default form submission behavior
+function validateUser(email, password) {
+  const users = JSON.parse(localStorage.getItem('usersDB')) || [];
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  return users.some(user => user.email === email && user.password === password);
+}
 
-    const newUsers = {
-      users: [
-        {
-          email: 'luis@gmail.com',
-          password: '1234',
-        },
-      ],
-    };
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+  event.preventDefault();
 
-    localStorage.setItem('users', JSON.stringify(newUsers));
+  initUsersDB();
 
-    if (validateUser(email, password)) {
-      alert('Login successful');
-      window.location.href = '../Index/index.html';
-    } else {
-      alert('Invalid email or password');
-    }
-  });
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-// const newUsers = {
-//   users: [
-//     {
-//       email: "luis@gmail.com",
-//       password: "1234",
-//     }
-//   ],
-// };
-
-// localStorage.setItem("users", JSON.stringify(newUsers));
+  if (validateUser(email, password)) {
+    alert('Login successful');
+    window.location.href = '../Index/index.html';
+  } else {
+    alert('Invalid email or password');
+  }
+});
--- Register/registration.js
+++ Register/registration.js
@@ -1,53 +1,55 @@
-function userCreation(){
-    const newUsers = {
-  users: [
-    {
-      email: "luis@gmail.com",
-      password: "1234",
+function initUsersDB() {
+  if (!localStorage.getItem('usersDB')) {
+    localStorage.setItem('usersDB', JSON.stringify([]));
+  }
+}
+
+document
+  .getElementById('registrationForm')
+  .addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    initUsersDB();
+
+    const userEmail = document.getElementById('email').value;
+    const userName = document.getElementById('name').value;
+    const userPassword = document.getElementById('password').value;
+    
+    const user = { email: userEmail, name: userName, password: userPassword };
+
+    if (validateRegistration(userEmail)) {
+      alert('Email already registered');
+      return;
     }
-  ],
-};
 
-localStorage.setItem("usersDB", JSON.stringify(newUsers));
+    const prevUsers = JSON.parse(localStorage.getItem('usersDB'));
+    const newUsers = [...prevUsers, user];
+
+    localStorage.setItem('usersDB', JSON.stringify(newUsers));
+
+    alert('User registered successfully. Redirecting to login page.');
+
+    window.location.href = '../Login/login.html';
+  });
+
+function validateRegistration(email) {
+  const users = JSON.parse(localStorage.getItem('usersDB'));
+
+  if (users.length === 0) {
+    return false;
+  }
+
+  const emailVerification = users.find((user) => user.email == email);
+
+  return emailVerification ? true : false;
 }
 
+/*
 
+function validateRegistration(email) {
+  const users = JSON.parse(localStorage.getItem('usersDB')) || [];
 
-function register(){
-
-
-    const userEmail = document.getElementById("email").value;
-
-    validateRegistration(userEmail);
-
-    const userName = document.getElementById("name").value;
-
-    const userPassword = document.getElementById("password").value;
-
-    const user = {email:userEmail, name:userName, password:userPassword}
-
-    const prevUsers = JSON.parse(localStorage.getItem("usersDB")).users;
-
-    const newUsers = [...prevUsers,user]
-
-    localStorage.setItem("usersDB", JSON.stringify(newUsers))
-
+  return users.some((user) => user.email === email);
 }
 
-function validateRegistration(email){
-
-    const users = JSON.parse(localStorage.getItem("usersDB")).users;
-
-    // const emailVerification = users.find((user) => user.email == email);
-
-    // localStorage.setItem("verifyEmail", JSON.stringify(emailVerification))
-
-    console.log("hola")
-
-}
-
-
-
-
-
-
+*/
